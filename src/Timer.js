eaa67f1b3542760fa5a4e6af143704ce90148669
PrajaktaParit <prajaktatp21@gmail.com>
2025-11-16 13:43:07 +0530

    Timer seconds left calculation optimized

--- src/Timer.js
+++ src/Timer.js
@@ -1,32 +1,32 @@
-import { useEffect, useReducer, useState } from "react";
+import { useEffect, useReducer } from "react";
 export default function Timer({ setStatusFinish, seconds }) {
   function reducer(state, action) {
     switch (action.type) {
       case "dec":
+        const total = state.min * 60 + state.sec - 1;
         return {
           ...state,
-          min: state.sec - 1 === -1 ? state.min - 1 : state.min,
-          sec: state.sec - 1 ===-1? 59 : state.sec -1,
+          min: Math.floor(total / 60),
+          sec: total%60,
         };
       default:
         return { ...initialState };
     }
   }
-  const initialState = { sec: seconds%60, min: Math.floor(seconds/60) };
+  const initialState = { sec: seconds % 60, min: Math.floor(seconds / 60) };
   const [state, dispatch] = useReducer(reducer, initialState);
   const { sec, min } = state;
   useEffect(() => {
     const id = setInterval(() => {
       console.log("in interval");
-      
+
       if (min === 0 && sec === 1) {
-        setStatusFinish({type:"finish"});
-      }
-      else dispatch({ type: "dec", payload: 1 });
+        setStatusFinish({ type: "finish" });
+      } else dispatch({ type: "dec", payload: 1 });
     }, 1000);
 
-    return ()=> clearInterval(id)
-  }, [min,sec,setStatusFinish]);
+    return () => clearInterval(id);
+  }, [min, sec, setStatusFinish]);
 
   return (
     <div className="timer">
